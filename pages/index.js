6f666f7dcfea596f701e77214cea2e41ba6ec630
joaojorgeprog <joaojorge.prog@gmail.com>
2021-02-13 12:53:46 +0000

    OLARE 3

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function Home() {
     return (
         <div>
-            <h1>asdasd</h1>
+        <h1>{process.env.GITHUB_ID}asdasd</h1>
             <Contador />
         </div>
     )
