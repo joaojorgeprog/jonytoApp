362e41ecc91245d258dd967cec058d286ddfe74a
joaojorgeprog <joaojorge.prog@gmail.com>
2021-02-13 12:51:27 +0000

    OLARE 2

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function Home() {
     return (
         <div>
-            <h1>olare</h1>
+            <h1>asdasd</h1>
             <Contador />
         </div>
     )
